4c1e2012f4e6623e06faec373c1323580acedb73
Oisin Creaner <creanero@gmail.com>
2025-01-09 11:40:22 +0000

    Added newline and acknowledgement

--- week1/hello_world.c
+++ week1/hello_world.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main() {
+
+// Based on Parewa Labs Pvt. Ltd. 
+// Available online at
+// https://www.programiz.com/c-programming/examples/print-sentence
+// 2025-01-09
+
+int main() 
+{
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World!\n");
    return 0;
 }

be63bd708b2b8085ddd86eadf4cb98a379167f0d
Oisin Creaner <creanero@gmail.com>
2025-01-26 15:20:24 +0000

    included vector operations in serial form

---
+++ week3/vector_serial.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int check_args(int argc, char **argv);
+void initialise_vector(int vector[], int size, int initial);
+void print_vector(int vector[], int size);
+int sum_vector(int vector[], int size);
+
+int main(int argc, char **argv)
+{
+	// declare and initialise the numerical argument variable
+	int num_arg = check_args(argc, argv);
+
+	// creates a vector variable
+	int my_vector[num_arg];
+	// and initialises every element to zero
+	initialise_vector(my_vector, num_arg, 0);
+
+
+	// TODO: put some code here that makes a more meaningful vector
+
+	// sums the vector
+	int my_sum = sum_vector(my_vector, num_arg);
+
+	// prints the sum
+	printf("Sum: %d\n", my_sum);
+
+	return 0;
+}
+
+int sum_vector(int vector[], int size)
+{
+	// creates a variable to hold the sum
+	int sum = 0;
+
+	// iterates through the vector
+	for (int i = 0; i < size; i++)
+	{
+		// sets the elements of the vector to the initial value
+		sum += vector[i];
+	}
+
+	// returns the sum
+	return sum;
+}
+
+void initialise_vector(int vector[], int size, int initial)
+{
+	// iterates through the vector
+	for (int i = 0; i < size; i++)
+	{
+		// sets the elements of the vector to the initial value
+		vector[i] = initial;
+	}
+}
+
+void print_vector(int vector[], int size)
+{
+	// iterates through the vector
+	for (int i = 0; i < size; i++)
+	{
+		// prints the elements of the vector to the screen
+		printf("%d\n", vector[i]);
+	}
+}
+
+int check_args(int argc, char **argv)
+{
+	// declare and initialise the numerical argument
+	int num_arg = 0;
+
+	// check the number of arguments
+	if (argc == 2) // program name and numerical argument
+	{
+		// declare and initialise the numerical argument
+		num_arg = atoi(argv[1]);
+	}
+	else // the number of arguments is incorrect
+	{
+		// raise an error
+		fprintf(stderr, "ERROR: You did not provide a numerical argument!\n");
+		fprintf(stderr, "Correct use: %s [NUMBER]\n", argv[0]);
+
+		// and exit COMPLETELY
+		exit (-1);
+	}
+	return num_arg;
+}
+
